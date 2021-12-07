d88948555990cb40ab30a0c0b5c2f7e35a3c00d8
jmetz9 <jmetz9@my.wctc.edu>
2021-12-06 19:32:15 -0600

    Convert MovieLibrary application to use databases

---
+++ AppManager.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using MovieLibraryDatabase.Context;
+using MovieLibraryDatabase.DataModels;
+
+namespace MovieLibraryDatabase
+{
+    public class AppManager
+    {
+        public void Search(string title)
+        {
+            using (var db = new MovieContext())
+            {
+                int returnedMovies = 0;
+                var movies = db.Movies.Where(x => x.Title.ToLower().Contains(title.ToLower()));
+                foreach (var m in movies)
+                {
+                    Console.WriteLine($"Movie {m.Id}: {m.Title}, {m.ReleaseDate}");
+                    returnedMovies++;
+                }
+
+                if (returnedMovies == 0)
+                {
+                    Console.WriteLine("There are no movies with term -" + title + "- in the title");
+                }
+
+            }
+        }
+
+        public void AddMovie(string title)
+        {
+            using (var db = new MovieContext())
+            {
+                DateTime release = DateTime.Now;
+                var movie = new Movie
+                {
+                    Title = title,
+                    ReleaseDate = release
+                };
+                db.Movies.Add(movie);
+                db.SaveChanges();
+
+                Console.WriteLine(movie.Title + " has been added");
+            }
+        }
+
+        public void UpdateMovie(string title, string updateTo)
+        {
+            using (var db = new MovieContext())
+            {
+                var updateMovie = db.Movies.FirstOrDefault(x => x.Title == title);
+
+                try
+                {
+                    updateMovie.Title = updateTo;
+                    db.Movies.Update(updateMovie);
+                    db.SaveChanges();
+
+                    Console.WriteLine("Title " + title + " changed to: " + updateTo);
+                }
+                catch (System.NullReferenceException)
+                {
+                    Console.WriteLine("No movies with name: " + title);
+                }
+
+            }
+        }
+
+        public void DeleteMovie(string title)
+        {
+            using (var db = new MovieContext())
+            {
+                var deleteMovie = db.Movies.FirstOrDefault(x => x.Title == title);
+                
+                try
+                {
+                    
+                    System.Console.WriteLine($"Are you sure you want to delete the following movie?(Y = Yes, Anything Else = No) {deleteMovie.Id}: {deleteMovie.Title}, {deleteMovie.ReleaseDate}");
+                    string yesNo = Console.ReadLine();
+
+                    if(yesNo == "y"){
+                    Console.WriteLine("Deleting...");
+                    db.Movies.Remove(deleteMovie);
+                    db.SaveChanges();
+                    Console.WriteLine("Title: " + title + " has been deleted :(");
+                    }
+
+                }
+                catch (System.NullReferenceException)
+                {
+                    Console.WriteLine("No movies with name: " + title);
+                }
+                
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace MovieLibraryDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppManager appMan = new AppManager();
+            
+            string choice;
+            do
+            {
+                Console.WriteLine("1) Search Movie by title.");
+                Console.WriteLine("2) Add a movie to the database");
+                Console.WriteLine("3) Change a Movie's title");
+                Console.WriteLine("4) Delete a movie from the database");
+                Console.WriteLine("Enter any other key to exit.");
+                choice = Console.ReadLine();
+                if (choice == "1")
+                {
+                    Console.WriteLine("Enter your search term: ");
+                    string input = Console.ReadLine();
+                    appMan.Search(input);
+                }
+                else if (choice == "2")
+                {
+                    Console.WriteLine("Enter the movie title");
+                    string input = Console.ReadLine();
+                    appMan.AddMovie(input);
+                }
+                else if (choice == "3")
+                {
+                    Console.WriteLine("Enter movie title to change");
+                    string input = Console.ReadLine();
+                    Console.WriteLine("What would you like to change it to?");
+                    string change = Console.ReadLine();
+
+                    appMan.UpdateMovie(input, change);
+                }
+                else if (choice == "4")
+                {
+                    Console.WriteLine("Enter movie title to delete");
+                    string input = Console.ReadLine();
+
+                    appMan.DeleteMovie(input);
+                }
+                Console.WriteLine("Anything else? Y/N");
+                choice = Console.ReadLine();
+            } while (choice.ToLower() == "y");
+        }
+
+    }
+}
